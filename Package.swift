9c8d4924106c1b3021aab2441d9371e070d91df8
yukiny0811 <yukiny0811@gmail.com>
2023-10-15 23:27:08 +0900

    initial

--- Package.swift
+++ Package.swift
@@ -5,19 +5,28 @@ import PackageDescription
 
 let package = Package(
     name: "AppDevelopmentFeatures",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v10_15),
+        .macCatalyst(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AppDevelopmentFeatures",
             targets: ["AppDevelopmentFeatures"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/yukiny0811/DescriptionEnum", exact: "1.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AppDevelopmentFeatures"),
-        .testTarget(
-            name: "AppDevelopmentFeaturesTests",
-            dependencies: ["AppDevelopmentFeatures"]),
+            name: "AppDevelopmentFeatures",
+            dependencies: [
+                .product(name: "DescriptionEnum", package: "descriptionenum")
+            ]
+        ),
     ]
 )
--- Sources/AppDevelopmentFeatures/AppDevelopmentFeatures.swift
+++ Sources/AppDevelopmentFeatures/AppDevelopmentFeatures.swift
@@ -1,2 +1,30 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import DescriptionEnum
+
+@DescriptionEnum
+public enum AppDevFeatures: String, Codable, CaseIterable {
+    case Accelerate = "大規模計算ができます"
+    case Accessibility = "アクセシビリティ"
+    case Accounts = "ユーザーアカウントなどを制御できます"
+    case ActivityKit = "ロックスクリーンやDynamic Island上でLive Activityを実装できます"
+    case AddressBook = "ユーザーの連絡帳にアクセスできます"
+    case AGL = "OpenGLを使えます"
+    case AppClips = "AppClipを作れます"
+    case AppIntents = "SiriやShorcutなどと連動するアプリを作れます"
+    case AppStoreConnectAPI = "AppStoreConnectなどでの操作を自動化できます"
+    case AppKit = "MacOSアプリを作れます"
+    case AppleArchive = "ファイル圧縮に関するツールです"
+    case AppleCryptoKit = "暗号化に関するツールです"
+    case AppleMusicAPI = "AppleMusicに収録されている音楽などを検索できます"
+    case ARKit = "ARアプリを作れます"
+    case AssetsLibrary = "ユーザーのメディアライブラリにアクセスできます"
+    case AudioToolbox = "音に関するツールです"
+    case AudioUnit = "DAWのプラグインを作れます"
+    case Automator = "Automatorアクションを作れます"
+    case AVFAudio = "音を扱うライブラリです"
+    case AVFoundation = "映像を扱うライブラリです"
+    case AVKit = "映像を扱うツールです"
+}
